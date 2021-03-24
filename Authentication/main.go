57443b0a71bef1e9d7af13cf85af828e1fe9ea17
Anla3421 <d9633826@gmail.com>
2021-03-24 18:08:58 +0800

    update mysql

--- Authentication/main.go
+++ Authentication/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"server/Sql"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,12 +13,24 @@ func main() {
 	subRouterAuthenticated := router.Group("/api/v1/PersonId", gin.BasicAuth(gin.Accounts{
 		"admin": "admin",
 	}))
-	subRouterAuthenticated.GET("/:IdValue", GetMethod)
-	listenPort := "8080"
-	router.Run(":" + listenPort)
+	subRouterAuthenticated.Any("/:IdValue", PostMethod)
+	listenPort := ":8080"
+
+	router.GET("/api/hello", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(200, gin.H{
+			"message": "hello " + name,
+		})
+	})
+
+	router.GET("/api/test", func(c *gin.Context) {
+		c.JSON(200, Sql.Users())
+	})
+
+	router.Run(listenPort)
 }
 
-func GetMethod(c *gin.Context) {
+func PostMethod(c *gin.Context) {
 	fmt.Println("\n'GetMethod' called")
 	IdValue := c.Params.ByName("IdValue")
 	message := "GetMethod Called With Param: " + IdValue
